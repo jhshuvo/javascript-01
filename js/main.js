0a1ab91b5fee08f3e79b136a1aff4a4bf387ec85
JH Shuvo <111685088+jhshuvo@users.noreply.github.com>
2023-09-06 00:30:53 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+function answerOne(){
+    document.getElementById('one').innerHTML="1. United States, Mexico and Canada"
+   
+}
+
+
+function answertwo(){
+    document.getElementById('two').innerHTML="2. Emiliano Martinez"
+   
+}
+
+function answertre(){
+    document.getElementById('tre').innerHTML="3. Jules Rimet Trophy"
+   
+}
+function answerfour(){
+    document.getElementById('four').innerHTML="4. Croatia"
+   
+}
+function answerfive(){
+    document.getElementById('five').innerHTML="5. Lionel Messi"
+   
+}
+function answersix(){
+    document.getElementById('six').innerHTML="6. Spin"
+   
+}
+
+function answersvn(){
+    document.getElementById('svn').innerHTML="7. Newell's Old Boys"
+   
+}
+function answereht(){
+    document.getElementById('eht').innerHTML="8. Cristiano Ronaldo"
+   
+}
+function answernin(){
+    document.getElementById('nin').innerHTML="9. Leonel Messi"
+   
+}
+function answerten(){
+    document.getElementById('ten').innerHTML="10. Moroslav Klose"
+   
+}
+
+
